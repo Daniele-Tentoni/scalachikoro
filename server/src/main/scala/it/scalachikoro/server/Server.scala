c0e67810b1604ad4a1ddd076980042f9b64a2b21
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-09-20 17:10:39 +0200

    Programmatically run server on local address.

--- server/src/main/scala/it/scalachikoro/server/Server.scala
+++ server/src/main/scala/it/scalachikoro/server/Server.scala
@@ -1,15 +1,19 @@
 package it.scalachikoro.server
 
-import java.net.InetAddress
+import java.net.{DatagramSocket, InetAddress}
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Address, Deploy}
+import akka.remote.RemoteScope
 import akka.routing.FromConfig
 import it.scalachikoro.constants.ActorConstants.{LobbyActorName, ServerActorSystemName}
 import it.scalachikoro.server.lobby.LobbyActor
 
 object Server extends App {
-  val address: InetAddress = InetAddress.getLocalHost
-  println(f"Server is running on ${address.getHostAddress}")
+  val socket = new DatagramSocket()
+  socket.connect(InetAddress.getByName("8.8.8.8"), 10002)
+  val address = socket.getLocalAddress.getHostAddress
+  println(f"Server is running on $address")
+  val ip = new Address("akka", "sys", address, 1234)
   val server = ActorSystem(ServerActorSystemName)
-  server.actorOf(LobbyActor.props().withRouter(FromConfig()), name = LobbyActorName)
+  server.actorOf(LobbyActor.props().withRouter(FromConfig()).withDeploy(new Deploy(RemoteScope(ip))), name = LobbyActorName)
 }
