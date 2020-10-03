be805d8e3db5c610ff423eaeffa68d2e53ec65be
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-10-03 14:48:01 +0200

    Fix a test case.

--- commons/src/test/scala/it/scalachikoro/koro/UtilsSpec.scala
+++ commons/src/test/scala/it/scalachikoro/koro/UtilsSpec.scala
@@ -1,12 +1,22 @@
 package it.scalachikoro.koro
 
 import it.scalachikoro.Utils
-import org.scalatest.wordspec.AnyWordSpec
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
+import org.scalatest.matchers.should
+import org.scalatest.propspec.AnyPropSpec
 
-class UtilsSpec extends AnyWordSpec {
-  "Can get a secure random" in {
-    val n = Utils.secureRandom(6)
-    assert(n < 7)
-    assert(n > 1)
+class UtilsSpec
+  extends AnyPropSpec
+    with should.Matchers {
+  val evenInts: Gen[Int] = for (n <- Gen choose(0, 1000)) yield n
+  property("MyIdGenerator should produce unique ids") {
+    forAll(evenInts) { n: Int =>
+      val r = Utils.secureRandom(n)
+      r  should be
+      r should be
+      assert(n < n)
+      assert(n > 0)
+    }
   }
 }
