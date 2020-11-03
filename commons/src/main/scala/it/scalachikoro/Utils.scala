c757d2018a7d3ccc2c3ad75a9597d5a545fdf96d
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2020-11-03 11:02:22 +0100

    New functions.

--- commons/src/main/scala/it/scalachikoro/Utils.scala
+++ commons/src/main/scala/it/scalachikoro/Utils.scala
@@ -2,11 +2,42 @@ package it.scalachikoro
 
 import java.security.SecureRandom
 
+import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable.ArrayBuffer
+
 object Utils {
+  /**
+   * Get a secure random integer from Java.
+   *
+   * @param max max number to extract.
+   * @return secure random integer.
+   */
   def secureRandom(max: Int): Int = {
     val r: SecureRandom = new SecureRandom()
     val seed = r.generateSeed(32)
     r.nextBytes(seed)
     seed.hashCode() % max + 1
+  }
+
+  /**
+   * Returns a new collection of the same type in a randomly chosen order.
+   *
+   * @return the shuffled collection.
+   */
+  def secureShuffle[T, CC[X] <: TraversableOnce[X]](xs: CC[T])(implicit bf: CanBuildFrom[CC[T], T, CC[T]]): CC[T] = {
+    val buf = new ArrayBuffer[T] ++= xs
+
+    def swap(i1: Int, i2: Int) {
+      val tmp = buf(i1)
+      buf(i1) = buf(i2)
+      buf(i2) = tmp
+    }
+
+    for (n <- buf.length to 2 by -1) {
+      val k = secureRandom(n)
+      swap(n - 1, k)
+    }
+
+    (bf(xs) ++= buf).result()
   }
 }
--- commons/src/main/scala/it/scalachikoro/actors/MyActor.scala
+++ commons/src/main/scala/it/scalachikoro/actors/MyActor.scala
@@ -2,11 +2,23 @@ package it.scalachikoro.actors
 
 import akka.actor.Actor
 
-abstract class MyActor extends Actor{
+abstract class MyActor extends Actor {
+  /**
+   * Perform a function only if an Optional is not null.
+   *
+   * @param ref Optional object reference.
+   * @param f   Function to execute.
+   * @tparam T Type of optional.
+   */
   protected def withRef[T](ref: Option[T])(f: T => Unit): Unit = ref match {
     case Some(value) => f(value)
     case _ =>
   }
 
+  /**
+   * Print in the console a formatted string.
+   *
+   * @param m Message to print.
+   */
   protected def log(m: String): Unit = println(f"[${self.path}] $m")
 }
